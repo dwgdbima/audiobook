fe7017d8e08badc89976ae9718b1cd129f3f0335
dwgdbima <dewagedebima@gmail.com>
2023-11-30 15:29:59 +0800

    add to home screen

--- resources/views/web/customer/layout/main.blade.php
+++ resources/views/web/customer/layout/main.blade.php
@@ -38,10 +38,9 @@
         data-bs-delay="5000" data-bs-autohide="true">
         <div class="toast-body">
             <div class="content d-flex align-items-center mb-2"><img src="{{asset('dist/img/icons/icon-72x72.png')}}" alt="">
-                <h6 class="mb-0">Add to Home Screen</h6>
+                <h6 class="mb-0">Tambahkan ke layar utama</h6>
                 <button class="btn-close ms-auto" type="button" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div><span class="mb-0 d-block">Add Suha on your mobile home screen. Click the<strong class="mx-1">Add to
-                    Home Screen</strong>button &amp; enjoy it like a regular app.</span>
+            </div><span class="mb-0 d-block">Tambahkan Subiakto Audio Book ke halaman utaman ponsel anda. klik tombol <strong class="mx-1">Tambahkan ke layar utama</strong> di pengaturan.</span>
         </div>
     </div>
     @yield('content')
