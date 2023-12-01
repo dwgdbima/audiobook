1f667a857962d599a5f8aa0ec83b76b49018a204
dwgdbima <dewagedebima@gmail.com>
2023-12-01 16:44:36 +0800

    fixing pwa

--- resources/views/web/customer/layout/head.blade.php
+++ resources/views/web/customer/layout/head.blade.php
@@ -37,7 +37,8 @@
     <!-- Stylesheet -->
     <link rel="stylesheet" href="{{ asset('dist/style.css') }}">
     <!-- Web App Manifest -->
-    <link rel="manifest" href="{{ asset('dist/manifest.json') }}">
+    {{-- <link rel="manifest" href="{{ asset('dist/manifest.json') }}"> --}}
+    @laravelPWA
 
     {{-- Jquery cdn --}}
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
